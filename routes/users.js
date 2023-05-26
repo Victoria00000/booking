a2ff91921ab018ba27901e0fde8c9e7f08c3d748
Victoria <vvictoriaandres@gmail.com>
2023-05-26 15:32:31 -0300

    terminando los controllers de users y creando verifyToken.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,30 @@
 import { Router } from "express";
+import {
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
+// creating the router //
 export const routerUsers = Router();
 
-routerUsers.get("/", (req, res) => {
-  res.send("This is the users endpoint");
+// create user is in register controller (auth.js) //
+
+// check if user is logged in: GET //
+routerUsers.get("/check-auth", verifyToken, (req, res, next) => {
+  res.send("You are logged in");
 });
+
+// get all users: GET //
+routerUsers.get("/", getAllUsers);
+
+// get user by id: GET //
+routerUsers.get("/:id", getUser);
+
+// delete user by id: DELETE //
+routerUsers.delete("/:id", deleteUser);
+
+// update user by id: PUT //
+routerUsers.put("/:id", updateUser);
