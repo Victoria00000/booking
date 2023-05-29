8d5eabef007b79be2fb0e66007fb6aa6d18c72c3
Victoria <vvictoriaandres@gmail.com>
2023-05-29 02:06:53 -0300

    creando las verificaciones de logged in y de admin

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,26 @@ import {
   getUser,
   updateUser,
 } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 // creating the router //
 export const routerUsers = Router();
 
 // create user is in register controller (auth.js) //
 
-// check if user is logged in: GET //
+// check auth-token: GET //
 routerUsers.get("/check-auth", verifyToken, (req, res, next) => {
-  res.send("You are logged in");
+  res.send("Hi user, you are logged in. :D");
+});
+
+// check if user is logged: GET //
+routerUsers.get("/check-user/:id", verifyUser, (req, res, next) => {
+  res.send("Hi user, you are logged in, and you can delete this account. :D");
+});
+
+// check if user is admin: GET //
+routerUsers.get("/check-admin/:id", verifyAdmin, (req, res, next) => {
+  res.send("Hi admin, you are logged in, an you can delete all accounts. :D");
 });
 
 // get all users: GET //
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,14 +1,37 @@
 import jwt from "jsonwebtoken";
 import { createError } from "./error.js";
 
+// token verification //
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return next(createError(401, "You are not autorized!"));
+    return next(createError(401, "You are not allowed! :( "));
   }
   jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) return next(createError(403, "Invalid token!"));
+    if (err) return next(createError(403, "Invalid token! :( "));
     req.user = user;
     next();
+  });
+};
+
+// user verification //
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "You are not allowed :( "));
+    }
+  });
+};
+
+// admin verification //
+export const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "You are not admin :( "));
+    }
   });
 };
